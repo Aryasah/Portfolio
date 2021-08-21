d0cd61e141aa63098c591c4abeaf83885a617858
Arya Sah <aryasah30@gmail.com>
2021-08-22 02:57:48 +0530

    comiit

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -23,3 +23,20 @@ menuBtn.addEventListener('click',()=>{
         showMenu =false;
     }
 })
+
+
+
+const toggle = document.querySelector("#toggle");
+toggle.addEventListener("click", modeSwitch);
+
+let isLight = true;
+
+function modeSwitch() {
+  isLight = !isLight;
+  let root = document.body;
+  
+  isLight ? toggle.innerText = "🌞" : toggle.innerText = "🌚";
+  
+  root.classList.toggle("lightMode");
+}
+
